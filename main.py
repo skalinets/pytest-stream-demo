32af16a78abb8ca612b2a5efffb5972a8f317cb0
Serhii Kalinets <kalinets@gmail.com>
2024-08-12 18:25:52 +0300

    something works

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import httpx
 
 app = FastAPI()
 
+nfts = {}
+
 
 @app.get("/{id}")
-def get_nft(id):
-    return {"text": "foo"}
-    # return {"Hello": "World!!!!"}
+def get_nft(id: str):
+    global nfts
+    r = nfts.get(id)
+    if not r:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return r
+
+
+def get_cat_fact():
+    r = httpx.get("https://catfact.ninja/fact")
+    return r.json()["fact"]
 
 
 @app.post("/add/{item_id}")
-def mint_nft(item_id: int):
-    return {"item_id": item_id}
+def mint_nft(item_id: str):
+    global nfts
+    nfts[item_id] = {"text": get_cat_fact()}
--- test_api.py
+++ test_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 import httpx
+import pytest
 
 client = TestClient(app)
 
@@ -11,11 +12,30 @@ client = TestClient(app)
 #     assert r == 1
 
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": ["authorization", "host"],
+        "ignore_localhost": True,
+        "ignore_hosts": ["testserver"],
+        # "record_mode": "once",
+    }
+
+
+@pytest.mark.vcr()
 def test_mint_nft():
+    r3 = client.get("/1")
+    assert r3.status_code == 404
+
     r = client.post("/add/1")
     assert r.status_code == 200
+
     r1 = client.get("/1")
-
     assert r1.status_code == 200
-    assert r1.json()["text"] == "foo"
+    assert r1.json()["text"] == "Normal body temperature for a cat is 102 degrees F."
 
+
+@pytest.mark.vcr()
+def test_iana():
+    response = httpx.get('http://www.iana.org/domains/reserved')
+    assert response.status_code == 200
